6b8348f38a83a7b5d70ef30726aa6280956cf404
Robert Maury <daquinox@gmail.com>
2023-12-21 09:22:12 -0500

    Create Ghost resources

--- security_group.tf
+++ security_group.tf
@@ -25,3 +25,45 @@ resource "aws_security_group" "aws-demo-security-group" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_security_group" "ghost-demo" {
+  name   = "rmaury-aws-demo"
+  vpc_id = aws_vpc.aws-demo.id
+
+  tags = {
+    Name               = "rmaury-ghost-demo"
+    Owner              = "rmaury"
+    Purpose            = "aghost-demo"
+    "Created Date"     = "12-21-2023"
+    Department         = "SA"
+    "Estimated Expiry" = "12-21-2024"
+  }
+
+  ingress {
+    to_port     = 22
+    from_port   = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    to_port     = 23
+    from_port   = 23
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    to_port     = 3389
+    from_port   = 3389
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
